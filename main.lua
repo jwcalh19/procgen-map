6be168d26a4e73c65fa19e9e3f9685fc2ea2e456
Jon Stapleton <jstapleton@harrisonburg.k12.va.us>
2019-09-06 12:31:18 -0400

    Made 2d map

--- main.lua
+++ main.lua
@@ -1,16 +1,17 @@
 local Element = require 'element'
 
 map = {}
+pictures = {'grass.png', 'shore.png'}
 
 function love.load()
-    for x = 1, 10 do
-        map[x] = {}
-        for y = 1, 10 do
-            map[x][y] = Element:new('grass.png')
-            --map[x][y].x = ???
-            --map[x][y].y = ???
-        end
+  for x = 1, 6 do
+    map[x] = {}
+    for y = 1, 6 do
+      map[x][y] = Element:new(pictures[love.math.random(1, 2)])
+      map[x][y].x = (map[x][y].img:getWidth() / 2 * (x - 1)) - map[x][y].img:getWidth() / 2 * (y-1)
+      map[x][y].y = (map[x][y].img:getHeight() / 3.15 * (x - 1)) + map[x][y].img:getHeight() / 3.15 * (y-1)
     end
+  end
 end
 
 
@@ -20,9 +21,9 @@ end
 
 
 function love.draw()
-    for x = 1, #map do
-        for y = 1, #map do
-            love.graphics.draw(map[x][y].img, map[x][y].x, map[x][y].y, 0, 0.25)
-        end
+  for x = 1, 6 do
+    for y = 1, 6 do
+      love.graphics.draw(map[x][y].img, map[x][y].x, map[x][y].y)
     end
+  end
 end
